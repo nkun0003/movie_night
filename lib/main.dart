7b88b578f98c46d76aa77e7edb398b646ed8e795
nkun0003 <nkun0003@algonquinlive.com>
2024-11-30 16:30:51 -0500

    Created 3 screens, Welcome, Enter and share codes and display the main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
+import 'screens/welcome_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// import 'screens/share_code_screen.dart';
+// import 'screens/enter_code_screen.dart';
+void main() => runApp(MovieNight());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class MovieNight extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.brown,
       ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/', // The starting screen is the WelcomeScreen.
+      routes: {
+        '/': (context) => WelcomeScreen(),
+        // '/share_code': (context) => ShareCodeScreen(),
+        // '/enter_code': (context) => EnterCodeScreen(),
+      },
     );
   }
 }
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,1 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:platform_device_id/platform_device_id.dart'; // importing the package to get the device ID.
 
+class WelcomeScreen extends StatefulWidget {
+  @override
+  _WelcomeScreenState createState() =>
+      _WelcomeScreenState(); // creating a stateful widget for the dynamic content
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  String? deviceId; // here a variable to hold device ID.
+
+  @override
+  void initState() {
+    super.initState();
+    _getDeviceId(); // calling the function to fetch the device ID as soon as the screen loads
+  }
+
+  Future<void> _getDeviceId() async {
+    deviceId = await PlatformDeviceId
+        .getDeviceId; // fetching the device ID using the platform_device_id package.
+    print(
+        'Device ID: $deviceId'); // printing the device ID for debugging purposes.
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // here building the UI of the screen.
+    return Scaffold(
+      appBar: AppBar(title: Text('Movie Night!')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, '/share_code'),
+              child: Text('Get a Code'),
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, '/enter_code'),
+              child: Text('Enter a Code'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
